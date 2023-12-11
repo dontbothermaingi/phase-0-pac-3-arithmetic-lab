5c63a83de5bb85206ba9831c71180924d715847d
+ <+>
2023-12-11 21:11:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function add(a,b){
 function subtract(a,b){
     return a-b;
 }
-function multiply(a,b){
+function multiplies(a,b){
     return a*b;
 }
 function divide(a,b){
@@ -22,3 +22,20 @@ function makeInt(n){
 function preserveDecimal(n){
     return parseFloat(n,10)
 }
+
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); 
+
+const random = Math.floor(Math.random()*10) + 1;
+console.log(random);
+
+const num3 = 14;
+const num4 = 5;
+const mod = (num3 % num4);
+console.log(mod)
+
+const max = Math.max(12,13,14,15,16,17,18,19,20)
+console.log(max);
